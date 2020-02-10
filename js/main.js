18a35bb7497d534e2a35c3a2da61f4ea2b5839fd
Юлия Корнейчук <uliakorneychuk@MacBook-Air.local>
2020-02-10 23:57:58 +0300

    Заменяет querySelector на cloneNode в createPhoto()

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
-"use strict";
+'use strict';
 
 var COUNT = 25;
 var MESSAGES = [
-  "Всё отлично!",
-  "Вцелом, всё неплохо. Но не всё.",
-  "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
-  "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
-  "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
-  "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"
+  'Всё отлично!',
+  'Вцелом, всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 var NAMES = [
-  "Мадам Бовари",
-  "Профессор Преображенский",
-  "Акакий Акакиевич",
-  "Крошка Цахес",
-  "Мамаша Кураж",
-  "Сонечка Мармеладова"
+  'Мадам Бовари',
+  'Профессор Преображенский',
+  'Акакий Акакиевич',
+  'Крошка Цахес',
+  'Мамаша Кураж',
+  'Сонечка Мармеладова'
 ];
 var DESCRIPTIONS = [
-  "В самом деле, что может быть лучше – сидеть вечером с книжкой у камина? Горит лампа, в окна стучится ветер...",
-  "Никогда не читайте перед обедом советских газет...",
-  "Вряд ли где можно было найти человека, который так жил бы в своей должности",
-  "Я ничего на свете так не страшусь и не избегаю, как палящих лучей солнца",
-  "Коль торговать, не всё равно ли, свинцом иль сыром торговать?",
-  "Бог такого ужаса не допустит!"
+  'В самом деле, что может быть лучше – сидеть вечером с книжкой у камина? Горит лампа, в окна стучится ветер...',
+  'Никогда не читайте перед обедом советских газет...',
+  'Вряд ли где можно было найти человека, который так жил бы в своей должности',
+  'Я ничего на свете так не страшусь и не избегаю, как палящих лучей солнца',
+  'Коль торговать, не всё равно ли, свинцом иль сыром торговать?',
+  'Бог такого ужаса не допустит!'
 ];
 
-// Генерируем случайное число
-var getRandomNumber = function(minNumber, maxNumber) {
+var getRandomNumber = function (minNumber, maxNumber) {
   var randomNum = Math.floor(Math.random() * maxNumber);
   return randomNum > minNumber ? randomNum : minNumber;
 };
@@ -37,7 +36,7 @@ var createPhotoObj = function (index) {
     url: 'photos/' + (index + 1) + '.jpg',
     description: DESCRIPTIONS[getRandomNumber(0, DESCRIPTIONS.length)],
     likes: getRandomNumber(15, 200),
-    comments:[
+    comments: [
       {
         avatar: 'img/avatar-' + getRandomNumber(1, 6) + '.svg',
         message: MESSAGES[getRandomNumber(0, MESSAGES.length)],
@@ -50,7 +49,7 @@ var createPhotoObj = function (index) {
       }
     ]
   };
-  return photoCard
+  return photoCard;
 };
 
 var createPhotosData = function () {
@@ -59,29 +58,22 @@ var createPhotosData = function () {
     data.push(createPhotoObj(i));
   }
   return data;
-}
-
+};
 
 var sample = createPhotosData();
-console.log(sample);
 
 var createPhoto = function (photoCard) {
   var pictureTemplate = document.querySelector('#picture');
-  var picture = pictureTemplate.content.querySelector('.picture');
-  //var photoElement = picture.cloneNode(true);
-console.log(picture.querySelector('.picture__img'), photoCard);
-  picture.querySelector('.picture__img').src= photoCard.url;
-
-  picture.querySelector('.picture__info');
-  picture.querySelector('.picture__comments');
-  picture.querySelector('.picture__likes');
+  var picture = pictureTemplate.content.cloneNode(true);
+  picture.querySelector('.picture__img').src = photoCard.url;
+  picture.querySelector('.picture__comments').textContent = photoCard.comments.length;
+  picture.querySelector('.picture__likes').textContent = photoCard.likes;
 
   return picture;
 };
 
 var renderPhotos = function (photosData) {
   var pictures = document.querySelector('.pictures');
-  var photoElement = document.querySelector('#picture');
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < photosData.length; i++) {
@@ -90,8 +82,4 @@ var renderPhotos = function (photosData) {
   pictures.appendChild(fragment);
 };
 
-renderPhotos (sample);
-
-// 1. Объявить функцию генерации случайных данных
-// 2. Объявить функцию создания DOM-элемента на основе JS-объекта
-// 3. Объявить функцию заполнения блока DOM-элементами на основе массива JS-объектов
+renderPhotos(sample);
